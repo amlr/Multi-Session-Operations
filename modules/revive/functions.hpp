2122d17b8396eb398deba316e30f2fa044f47097
Daniel Cousens <daniel350@bigpond.com>
2010-10-09 14:48:13 +1100

    mod revive

---
+++ modules/revive/functions.hpp
@@ -0,0 +1,11 @@
+class revive {
+	file = "modules\revive";
+	class functions {
+		class init {};
+		class conscious {};
+		class unconscious {};
+		class damage {};
+		class handle_events {};
+		class heal {};
+	};
+};
