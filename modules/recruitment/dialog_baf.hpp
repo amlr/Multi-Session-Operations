43063db38cc9abedd806a169027ea86777112cde
Wolffy <wolff_borg@yahoo.com.au>
2010-11-14 01:15:17 +1100

    Removed generic AI recruitment dialog and replaced with specific factions.

---
+++ modules/recruitment/dialog_baf.hpp
@@ -0,0 +1,37 @@
+class RMM_ui_recruitment { // by Rommel
+	idd = 80506;
+	movingEnable = 1;
+	enableSimulation = 1;
+
+	class controls {
+		class Background : CUI_Frame {
+			y = CUI_Row_Y(0);
+			h = CUI_Row_DY(0,4);
+			w = CUI_Box_W;
+		};
+		class Caption : CUI_Caption {
+			text = "Recruitment";
+			y = CUI_Row_Y(0);
+			h = CUI_Row_DY(0,1);
+			w = CUI_Box_W;
+		};
+		class SoldierWB : CUI_Button {
+			text = "Regular";
+			w = CUI_Box_W;
+			y = CUI_Row_Y(1);
+			h = CUI_Row_DY(0,1);
+			action = "if ({!isplayer _x} count (units player) < 6) then {(group player) createunit [""BAF_Soldier_MTP"", [player, 50] call CBA_fnc_randPos, [], 0, ""FORM""]};";
+			default = true;
+		};
+		class SoldierMG : SoldierWB {
+			text = "Machinegunner";
+			y = CUI_Row_Y(2);
+			action = "if ({!isplayer _x} count (units player) < 6) then {(group player) createunit [""BAF_Soldier_MG_MTP"", [player, 50] call CBA_fnc_randPos, [], 0, ""FORM""]};";
+		};
+		class SoldierAT : SoldierWB {
+			text = "Antitank";
+			y = CUI_Row_Y(3);
+			action = "if ({!isplayer _x} count (units player) < 6) then {(group player) createunit [""BAF_Soldier_AT_MTP"", [player, 50] call CBA_fnc_randPos, [], 0, ""FORM""]};";
+		};
+	};
+};
