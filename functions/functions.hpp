9cff48a65fc2429bbf9093419dd5ff0873e504d3
Wolffy <wolff_borg@yahoo.com.au>
2010-11-14 01:32:33 +1100

    It looked like Rommel was moving all his functions to CBA, so I've temporarily commented out everything that wasn't in use and implemented CBA_fnc_ExMP until its released through CBA proper.

--- functions/functions.hpp
+++ functions/functions.hpp
@@ -1,22 +1,26 @@
 class RMM {
 	class client {
-		class taskhint {};
-		class taskupdate {};
+//		class taskhint {};
+//		class taskupdate {};
 	};
 	class common {
-		class ExMP {};
-		class failSafeLS {};
-		class isAlive {};
-		class landForTime {};
-		class supplyDrop {};
+//		class failSafeLS {};
+//		class isAlive {};
+//		class landForTime {};
+//		class supplyDrop {};
 	};
 	class server {
-		class spawnVehiclePath {};
-		class taskDefend {};
+//		class spawnVehiclePath {};
+//		class taskDefend {};
 	};
 };
 class BIS {
 	class server {
 		class dynocreate {};
+	};
+};
+class CBA {
+	class common {
+		class ExMP {};
 	};
 };
