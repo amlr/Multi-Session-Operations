f847842816b7f9e05470578207d175185effd491
Wolffy <wolff_borg@yahoo.com.au>
2010-11-21 19:48:49 +1100

    Changed debug by default and common MSO_FACTIONS for factions. Remove WIP marker as tested.

--- modules/zora/settings.hpp
+++ modules/zora/settings.hpp
@@ -1,6 +1,6 @@
-_logic setvariable ["debug",			false	];
+_logic setvariable ["debug",			true	];
 _logic setvariable ["bordersize", 		10000	];
-_logic setvariable ["factionlist", 		["BIS_TK"]	];
+_logic setvariable ["factionlist", 		MSO_FACTIONS	];
 _logic setvariable ["search_radius",	500	];
 _logic setvariable ["maxgroups",		1		];
 _logic setvariable ["mindist",			1400	];
