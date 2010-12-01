2cbc29cebaa369de56e2b79ab9022248442ff4cc
Wolffy <wolff_borg@yahoo.com.au>
2010-12-01 21:20:13 +1100

    Removed redundant functions.

--- functions/functions.hpp
+++ functions/functions.hpp
@@ -1,26 +1,11 @@
 class RMM {
-	class client {
-//		class taskhint {};
-//		class taskupdate {};
-	};
 	class common {
 		class ExMP {};
-//		class failSafeLS {};
-//		class isAlive {};
-//		class landForTime {};
-//		class supplyDrop {};
-	};
-	class server {
-//		class spawnVehiclePath {};
-//		class taskDefend {};
 	};
 };
 class CRB {
 	class common {
 		class selectRandomBias {};
-	};
-	class server {
-//		class initLocations {};
 	};
 };
 class BIS {
