9760f472bdd376cbac345209627d5dd0777b8a5e
Wolffy <wolff_borg@yahoo.com.au>
2010-12-31 18:41:49 +1100

    Renamed files cpp to hpp

---
+++ modules/dogs/cfgsounds.hpp
@@ -0,0 +1,15 @@
+class scream {
+	name = "scream";
+	sound[] = {modules\dogs\sounds\scream.ogg, 0.5, 1.0};
+	titles[] = {};
+};
+class dog_maul01 {
+	name = "dog_maul01";
+	sound[] = {modules\dogs\sounds\dog_maul01.ogg,0.5,1};
+	titles[] = {};
+};
+class dog_yelp {
+	name = "dog_yelp01";
+	sound[] = {modules\dogs\sounds\dog_yelp,0.5,1};
+	titles[] = {};
+};
