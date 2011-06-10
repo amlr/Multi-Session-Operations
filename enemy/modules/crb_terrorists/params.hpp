d3ed782fc3866c735aa866e5d80fd17c68075c46
Wolffy <wolff_borg@yahoo.com.au>
2011-06-11 00:07:39 +1000

    - Renamed parameters

--- enemy/modules/crb_terrorists/params.hpp
+++ enemy/modules/crb_terrorists/params.hpp
@@ -1,12 +1,12 @@
 class crb_tc_markers {
-        title = "TCells Insurgency Style Markers"; 
+        title = "Enemy - TCells Insurgency Style Markers"; 
         values[]= {0,1}; 
         texts[]= {"Off","On"}; 
         default = 1;
 };
 
 class crb_tc_intensity {
-        title = "TCells Recruitment Intensity per Hour"; 
+        title = "Enemy - TCells Recruitment per Hour"; 
         values[]= {0.25,0.5,1,2,3}; 
         texts[]= {"4 Recruits","2 Recruits","1 Recruit","0.5 Recruit","0.33 Recruit"}; 
         default = 1;
