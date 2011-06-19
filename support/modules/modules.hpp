38f0f4b2ef03c0c6236cff77464a7990edf74948
wolffy.au <wolff_borg@yahoo.com.au>
2011-06-20 08:03:07 +1000

    - Moved modules to stable

--- support/modules/modules.hpp
+++ support/modules/modules.hpp
@@ -1,18 +1,17 @@
 // - Tested
 #define RMM_AAR
 #define RMM_CAS
-#define RMM_CNSTRCT
+#define RMM_CASEVAC
+#define CRB_TWNMGR
 #define CRB_FLIPPABLE
 #define RMM_JIPMARKERS
 #define RMM_LOGBOOK
+#define R3F_LOGISTICS
 #define RMM_RECRUITMENT
 #define RMM_REVIVE
 #define RMM_TASKS
-#define CRB_TWNMGR
 #define RMM_TYRES
-#define R3F_LOGISTICS
+#define WHB_MULTISPAWN
 
 // WIP
-#define WHB_MULTISPAWN
-#define RMM_CASEVAC
 //#define GC_PACK_COW
