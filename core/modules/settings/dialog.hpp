c637bdae75f0e428c412816397c87fb864e1303a
Wolffy <wolff_borg@yahoo.com.au>
2011-10-03 18:27:12 +1100

    - Fixed TerrainGrid formula

--- core/modules/settings/dialog.hpp
+++ core/modules/settings/dialog.hpp
@@ -2,7 +2,7 @@ class RMM_ui_settings {
 	idd = 80508;
 	movingEnable = 1;
 	enableSimulation = 1;
-	onLoad = "[] spawn {if (isnil ""terraindetail"") then {terraindetail = 1}; ctrlSetText [1,format['%1/%2 m', viewdistance, settings_maxvd]]; ctrlSetText [3,format['%1/%2', terraindetail, 5]]; sliderSetRange [2, 500, settings_maxvd];sliderSetRange [4, 0, 5];sliderSetSpeed [2, 50, 100];sliderSetSpeed [4, 1, 1];sliderSetPosition [2, viewdistance];sliderSetPosition [4, terraindetail];};";
+	onLoad = "[] spawn {if (isnil ""terraindetail"") then {terraindetail = 2}; ctrlSetText [1,format['%1/%2 m', viewdistance, settings_maxvd]]; ctrlSetText [3,format['%1/%2', terraindetail, 5]]; sliderSetRange [2, 500, settings_maxvd];sliderSetRange [4, 1, 5];sliderSetSpeed [2, 50, 100];sliderSetSpeed [4, 1, 1];sliderSetPosition [2, viewdistance];sliderSetPosition [4, terraindetail];};";
 	
 	class controls {
 		class Background : CUI_Frame {
@@ -36,7 +36,7 @@ class RMM_ui_settings {
 		class TDSlider : CUI_Slider {
 			idc = 4;
 			y = CUI_Box_Row(0,5.5);
-			onSliderPosChanged = "_this spawn {private ""_terraindetail""; _terraindetail = round(_this select 1); terraindetail = _terraindetail;  ctrlSetText [3,format['%1/%2', terraindetail, 5]]; sleep 3/4; if (terraindetail == _terraindetail) then {setterraingrid ((-10 * terraindetail + 50) max 1);}};";
+			onSliderPosChanged = "_this spawn {private ""_terraindetail""; _terraindetail = round(_this select 1); terraindetail = _terraindetail;  ctrlSetText [3,format['%1/%2', terraindetail, 5]]; sleep 3/4; if (terraindetail == _terraindetail) then {setterraingrid ([50, 25, 12.5, 6.25, 3.125] select (terrainDetail - 1));}};";
 		};
 	};
 };
