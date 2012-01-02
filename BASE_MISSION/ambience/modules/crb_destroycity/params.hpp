afb7a78004bc733a464a9234fc812dced1431cb6
Tupolov <tupolov73@gmail.com>
2012-01-02 17:04:13 +0000

    - Updated mission param formatting... again.

--- BASE_MISSION/ambience/modules/crb_destroycity/params.hpp
+++ BASE_MISSION/ambience/modules/crb_destroycity/params.hpp
@@ -1,17 +1,17 @@
 class destroyCity {
-        title = "		Ambient - Destroy City"; 
+        title = "        Ambient - Destroy City"; 
         values[]= {0}; 
         texts[]= {" "}; 
         default = 0;
 };
 class destroyCityIntensity {
-        title = "    		Enable Destroyed Cities"; 
+        title = "            Enable Destroyed Cities"; 
         values[]= {0,1,2,3}; 
         texts[]= {"Off", "Low", "Med", "High"}; 
         default = 0;
 };
 class destroyCityFire {
-        title = "    		Enable Destroyed City Building Fires"; 
+        title = "            Enable Destroyed City Building Fires"; 
         values[]= {0,1}; 
         texts[]= {"Off", "On"}; 
         default = 0;
