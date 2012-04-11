318e84d1f230c22969c933e6a632cc7bd9ddde45
Tupolov <tupolov73@gmail.com>
2012-04-11 21:47:40 +0100

    [MISSIONS] - Updated Lingor Enemy Module definition

--- missions/13_a2co_coop_usmc_vs_ru_gue.lingor/enemy/modules/modules.hpp
+++ missions/13_a2co_coop_usmc_vs_ru_gue.lingor/enemy/modules/modules.hpp
@@ -2,9 +2,12 @@
 #define MSOFACTIONS
 #define CRB_CONVOYS
 #define RMM_ENEMYPOP
-//#define CRB_TERRORISTS
+#define CRB_TERRORISTS
 #define RMM_ZORA
 
 // - To Test
+#define TUP_IED
 #define WICT_ENEMYPOP
+#define RYD_HAC
+#define ROY_PATROLOPS
 //#define BIS_WARFARE
