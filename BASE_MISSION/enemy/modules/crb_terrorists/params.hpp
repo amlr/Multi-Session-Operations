5e1c64df22d1490308e1d1556d0f738952c95e91
highhead <1enterprise2>
2012-05-06 14:10:51 +0200

    [ENEMY] Turned off terrorists until moved to clientside or fixed (breaks initalizing enemypop-modules)

--- BASE_MISSION/enemy/modules/crb_terrorists/params.hpp
+++ BASE_MISSION/enemy/modules/crb_terrorists/params.hpp
@@ -2,7 +2,7 @@ class crb_tc_intensity {
         title = "    Civilian Terrorist Cells Recruitment per Hour (relies on Ambient Civs)"; 
         values[]= {0,1,2,3,4,5}; 
         texts[]= {"None","8 Recruits","4 Recruits","2 Recruits","1 Recruit","0.5 Recruit"}; 
-        default = 1;
+        default = 0;
 };
 class crb_tc_markers {
         title = "        Civilian Terrorist Insurgency Style Markers"; 
