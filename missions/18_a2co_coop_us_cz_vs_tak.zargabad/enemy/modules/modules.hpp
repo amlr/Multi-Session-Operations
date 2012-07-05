d2d52329d04b977ab62ea02f7d0c106b2a83d945
highhead <1enterprise2>
2012-07-05 17:51:38 +0200

    [MISSIONS] Updated Zargabad COOP Mission with all Support modules (default as is base) and included CQB

--- missions/18_a2co_coop_us_cz_vs_tak.zargabad/enemy/modules/modules.hpp
+++ missions/18_a2co_coop_us_cz_vs_tak.zargabad/enemy/modules/modules.hpp
@@ -1,16 +1,14 @@
 // - Tested
 #define MSOFACTIONS
 //#define CRB_CONVOYS
-//#define RMM_ENEMYPOP
+#define RMM_ENEMYPOP
 #define CRB_TERRORISTS
 #define RMM_ZORA
 
 // - To Test
 #define TUP_IED
-
-//#define WICT_ENEMYPOP
-//#define RYD_HAC
+#define WICT_ENEMYPOP
+#define RYD_HAC
 #define ROY_PATROLOPS
-
-
+#define CQB_POP
 //#define BIS_WARFARE
