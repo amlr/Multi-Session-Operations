b42d95d811e4ec767b8ef0ce86b70056aac12104
highhead <1enterprise2>
2013-03-04 20:14:42 +0100

    [AMBIENCE] Turned of EMS by default (not stable for 1.62)

--- BASE_MISSION/ambience/modules/crb_emergency/params.hpp
+++ BASE_MISSION/ambience/modules/crb_emergency/params.hpp
@@ -2,7 +2,7 @@ class Emergency {
         title = "    Enable Emergency Services"; 
         values[]= {0, 1}; 
         texts[]= {"Off","On"}; 
-        default = 1;
+        default = 0;
 };
 class EmergencyLocality {
 	title = "        Emergency Locality";
